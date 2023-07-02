4451b1948fd25b720af665f8d0353ce8b84bad87
Ciprian <cozma_ciprian19@yahoo.com>
2023-07-02 23:15:28 +0300

    added star component

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import Star from "./Star";
 
 const containerStyle = {
   display: "flex",
   alignItems: "center",
-  gap: "16px",
 };
 
 const starContainerStyle = {
@@ -16,15 +16,23 @@ const textStyle = {
   margin: "0",
 };
 
-function StarRating({ rating = 5 }) {
+function StarRating({ maxRating = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
-        {Array.from({ length: rating }, (_, i) => (
-          <span>S{i + 1}</span>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            onClick={() => setRating(i + 1)}
+            onMouseEnter={() => setTempRating(i + 1)}
+            onMouseLeave={() => setTempRating(0)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+          />
         ))}
       </div>
-      <p style={textStyle}>10</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
